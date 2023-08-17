87618762d82a2bc375160a6442587b4b5ca67711
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2023-08-17 16:54:28 -0300

    Melhorando o formulário de contato

--- js/dialog.js
+++ js/dialog.js
@@ -1,37 +1,46 @@
 // Variáveis
+var conteudoForaDialog = document.querySelector('#conteudoForaDialog');
+var video = document.querySelector('video');
+
 var btnAbreDialog = document.querySelector('#abreDialog');
 var dialog = document.querySelector('.dialogNewsletter');
 var dialogBody = document.querySelector('.dialogNewsletter-body');
 var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
-var conteudoForaDialog = document.querySelector('#conteudoForaDialog');
-var video = document.querySelector('video');
 
-btnAbreDialog.style.display = 'block';
-
+btnAbreDialog.style.display = 'block'
 
 // Quando abrir a dialog...
 btnAbreDialog.addEventListener('click', function() {
   dialog.classList.add('dialogNewsletter--aberto');
-  document.querySelector('.dialogNewsletter-campo').focus();
+  document.querySelector('.dialogNewsletter-label').focus();
   conteudoForaDialog.inert = true;
-  video.removeAttribute('controls');
+
+  document.querySelector('video').removeAttribute('controls'); 
 });
 
 function fechandoDialog() {
   document.activeElement.blur();
-  dialog.classList.remove('dialogNewsletter--aberto');
+  dialog.classList.remove('dialogNewsletter--aberto'); 
   conteudoForaDialog.inert = false;
   btnAbreDialog.focus();
-  video.setAttribute('controls', true);
+  document.querySelector('video').setAttribute('controls', 'true');
 }
 
 // Listeners
 document.querySelector('.dialogNewsletter-fechar').addEventListener('click', fechandoDialog);
-
 dialogOverlay.addEventListener('click', fechandoDialog);
+
 document.addEventListener('keyup', function(evento) {
   if (evento.keyCode == 27) {
-    fechandoDialog();
-  }  
-  
-});
+      fechandoDialog()
+  }
+})
+
+
+
+
+
+
+
+
+
