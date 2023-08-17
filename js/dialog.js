5d40de8e1107efc2a56028cbcc73cacb9ff0c529
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2023-08-17 15:22:42 -0300

    Deixando a modal mais acessível

--- js/dialog.js
+++ js/dialog.js
@@ -4,6 +4,7 @@ var dialog = document.querySelector('.dialogNewsletter');
 var dialogBody = document.querySelector('.dialogNewsletter-body');
 var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
 var conteudoForaDialog = document.querySelector('#conteudoForaDialog');
+var video = document.querySelector('video');
 
 btnAbreDialog.style.display = 'block';
 
@@ -13,14 +14,24 @@ btnAbreDialog.addEventListener('click', function() {
   dialog.classList.add('dialogNewsletter--aberto');
   document.querySelector('.dialogNewsletter-campo').focus();
   conteudoForaDialog.inert = true;
+  video.removeAttribute('controls');
 });
 
 function fechandoDialog() {
   document.activeElement.blur();
   dialog.classList.remove('dialogNewsletter--aberto');
   conteudoForaDialog.inert = false;
-  btnAbreDialog.focus()     ;
+  btnAbreDialog.focus();
+  video.setAttribute('controls', true);
 }
 
 // Listeners
 document.querySelector('.dialogNewsletter-fechar').addEventListener('click', fechandoDialog);
+
+dialogOverlay.addEventListener('click', fechandoDialog);
+document.addEventListener('keyup', function(evento) {
+  if (evento.keyCode == 27) {
+    fechandoDialog();
+  }  
+  
+});
