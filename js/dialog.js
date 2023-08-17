a7a8c77c79babe64929bb966e1d375cd5e2e48a8
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2023-08-17 13:30:13 -0300

    Ajustando o dialog do Newsletter

--- js/dialog.js
+++ js/dialog.js
@@ -3,16 +3,23 @@ var btnAbreDialog = document.querySelector('#abreDialog');
 var dialog = document.querySelector('.dialogNewsletter');
 var dialogBody = document.querySelector('.dialogNewsletter-body');
 var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
+var conteudoForaDialog = document.querySelector('#conteudoForaDialog');
+
+btnAbreDialog.style.display = 'block';
 
 
 // Quando abrir a dialog...
 btnAbreDialog.addEventListener('click', function() {
   dialog.classList.add('dialogNewsletter--aberto');
+  document.querySelector('.dialogNewsletter-campo').focus();
+  conteudoForaDialog.inert = true;
 });
 
 function fechandoDialog() {
   document.activeElement.blur();
-  dialog.classList.remove('dialogNewsletter--aberto');     
+  dialog.classList.remove('dialogNewsletter--aberto');
+  conteudoForaDialog.inert = false;
+  btnAbreDialog.focus()     ;
 }
 
 // Listeners
